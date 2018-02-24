5897cab3ac2759fe59da2aa8cd744b46641f16a2
David Stone <david@stabbylambda.com>
2018-02-24 11:10:04 -0800

    Make sure files get prettier-ed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import * as fanControl from 'quietcool';
-import Koa from 'koa';
-import Router from 'koa-router';
-import * as _ from 'lodash';
-const logger = require('koa-logger')
-const bodyParser = require('koa-bodyparser')
+import * as fanControl from "quietcool";
+import Koa from "koa";
+import Router from "koa-router";
+import * as _ from "lodash";
+const logger = require("koa-logger");
+const bodyParser = require("koa-bodyparser");
 
 const app = new Koa();
 app.use(logger());
@@ -13,46 +13,49 @@ const router = new Router();
 
 const port = process.env.PORT || 3001;
 
-router.get('/fans/:ip', async (ctx) => {
-    let ip = ctx.params.ip
-    let fans = await fanControl.listFansWithInfo(ip).toArray().toPromise();
+router.get("/fans/:ip", async ctx => {
+  let ip = ctx.params.ip;
+  let fans = await fanControl
+    .listFansWithInfo(ip)
+    .toArray()
+    .toPromise();
 
-    let sorted = _.chain(fans).sortBy(f => f.info.hubid);
+  let sorted = _.chain(fans).sortBy(f => f.info.hubid);
 
-    ctx.body = { fans: sorted };
+  ctx.body = { fans: sorted };
 });
 
-const getId = (ctx) => {
-    let {ip, uid} = ctx.params;
-    return {ip, uid};
-}
+const getId = ctx => {
+  let { ip, uid } = ctx.params;
+  return { ip, uid };
+};
 
-router.post('/fans/:ip/:uid/on', async (ctx) => {
-    let id = getId(ctx);
-    let response = await fanControl.turnFanOn(id).toPromise();
-    ctx.body = response;
+router.post("/fans/:ip/:uid/on", async ctx => {
+  let id = getId(ctx);
+  let response = await fanControl.turnFanOn(id).toPromise();
+  ctx.body = response;
 });
 
-router.post('/fans/:ip/:uid/off', async (ctx) => {
-    let id = getId(ctx);
-    let response = await fanControl.turnFanOff(id).toPromise();
-    ctx.body = response;
+router.post("/fans/:ip/:uid/off", async ctx => {
+  let id = getId(ctx);
+  let response = await fanControl.turnFanOff(id).toPromise();
+  ctx.body = response;
 });
 
-router.post('/fans/:ip/:uid/setCurrentSpeed', async (ctx) => {
-    let id = getId(ctx);
-    let {speed} = ctx.request.body;
+router.post("/fans/:ip/:uid/setCurrentSpeed", async ctx => {
+  let id = getId(ctx);
+  let { speed } = ctx.request.body;
 
-    let response = await fanControl.setCurrentSpeed(id, speed).toPromise();
-    ctx.body = response;
+  let response = await fanControl.setCurrentSpeed(id, speed).toPromise();
+  ctx.body = response;
 });
 
-router.post('/fans/:ip/:uid/updateSpeeds', async (ctx) => {
-    let id = getId(ctx);
-    let {speeds} = ctx.request.body;
+router.post("/fans/:ip/:uid/updateSpeeds", async ctx => {
+  let id = getId(ctx);
+  let { speeds } = ctx.request.body;
 
-    let response = await fanControl.updateFanSpeeds(id, speeds).toPromise();
-    ctx.body = response;
+  let response = await fanControl.updateFanSpeeds(id, speeds).toPromise();
+  ctx.body = response;
 });
 
 app.use(router.routes());
