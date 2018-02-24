4f69f90241151669c0848ec259ac9c484cd33b0c
David Stone <david@stabbylambda.com>
2018-02-24 11:44:16 -0800

    Fix errors

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ router.post("/fans/:ip/:uid/off", async ctx => {
 
 router.post("/fans/:ip/:uid/setCurrentSpeed", async ctx => {
   let id = getId(ctx);
-  let { speed } = ctx.request.body;
+  let { speed } = (<any>ctx.request).body;
 
   let response = await fanControl.setCurrentSpeed(id, speed).toPromise();
   ctx.body = response;
@@ -52,7 +52,7 @@ router.post("/fans/:ip/:uid/setCurrentSpeed", async ctx => {
 
 router.post("/fans/:ip/:uid/updateSpeeds", async ctx => {
   let id = getId(ctx);
-  let { speeds } = ctx.request.body;
+  let { speeds } = (<any>ctx.request).body;
 
   let response = await fanControl.updateFanSpeeds(id, speeds).toPromise();
   ctx.body = response;
